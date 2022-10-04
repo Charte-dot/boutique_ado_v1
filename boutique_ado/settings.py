65523141097bd3a975940869ab8a595dbef8feec
Caroline Harte <caroline.harte@yahoo.com>
2022-10-04 10:39:24 +0000

    Added toast messages to base.html

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.sessions.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
 
     # Needed to login by username in Django admin, regardless of `allauth`
